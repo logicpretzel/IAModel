02a4d6ddc74d7d869e0352bf7cceda0c23cdec68
logicpretzel <dunham5@mail.nmc.edu>
2017-02-13 09:36:45 -0500

    Add project files.

---
+++ IAModel/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IAModel.MvcApplication" Language="C#" %>
---
+++ IAModel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IAModel.Startup))]
+namespace IAModel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
